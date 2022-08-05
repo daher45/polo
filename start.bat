4443c80e713a752ce91bf8761be9f0588a79cc1d
datpusheen123 <75307264+datpusheen123@users.noreply.github.com>
2022-08-05 10:40:01 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By AvishkarPatil" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator @AviRDP21 /add >nul
+net user administrator @push33n /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -16,8 +16,8 @@ echo  Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @AviRDP21
+echo Password: @push33n
 echo .
-echo  RDP By Avishkar Patil (+_+)
+echo  RDP By Push33n
 echo   Please Login to Your RDP !
 ping -n 10 127.0.0.1 >nul
